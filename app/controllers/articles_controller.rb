1bc6ecb4b838c380f69ec0ab5d63d2e248a9f566
Santhosh Sivan A <santhoshsivan18g207@gmail.com>
2022-06-10 18:00:17 +0530

    Ruby on Rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
   def index
   end
+  def contact
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-
-
+  root "articles#index"
+  get "/users", to: "users#details"
   get "/articles", to: "articles#index"
+  get "/contact", to: "articles#contact"
+  get "/about", to: "about#about"
 end
