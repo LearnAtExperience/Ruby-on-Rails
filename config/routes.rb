5eb4357dacc8a846d34a9131f330674bea766d27
Santhosh Sivan A <santhoshsivan18g207@gmail.com>
2022-06-22 14:59:43 +0530

    Ruby on Rails

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root "articles#index"
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
